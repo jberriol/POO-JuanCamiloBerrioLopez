065e9fdb8ff858e0ded4834d25d61b2ad7ce0493
jberriol <jberriol@unal.edu.co>
2023-08-19 14:45:59 -0500

    Segundo ejercicio terminado

--- src/Parcial1/POOER5.java
+++ src/Parcial1/POOER5.java
@@ -10,14 +10,14 @@ package Parcial1;
  */
 
 public class POOER5 {public static void main(String[] args){
-    double suma, x, y;
-    suma = 0;
-    x = 20;
-    y = 40;
-    suma = suma + x;
-    x = Math.pow(y,2) + x;
-    suma = suma + (x/y);
-    System.out.println("El valor de la suma es: " + suma);
+    double suma, x, y; //Defino variables
+    suma = 0; // suma en 0
+    x = 20; //x con valor
+    y = 40; // y con valor
+    suma = suma + x; //la suma cambia
+    x = Math.pow(y,2) + x; //utilizo la potencia de la libreria Math
+    suma = suma + (x/y); // Operacion final
+    System.out.println("El valor de la suma es: " + suma); //Imprimo resultado
     }
     
 }
