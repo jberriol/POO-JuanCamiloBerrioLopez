54e0f7b468dd3de57d7ffe5cf9b5ce9d260f2cd2
jberriol <jberriol@unal.edu.co>
2023-09-09 16:22:33 -0500

    Ejercicio 22 y 10 terminados

---
+++ src/Parcial3/POOEP22.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP22 {
+    public static void main(String args[]){
+        Formulario22 form = new Formulario22();
+        form.setVisible(true);
+    }
+}
---
+++ src/Parcial3/POOER10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOER10 {
+    public static void main(String args[]){
+        Formulario10 form = new Formulario10();
+        form.setVisible(true);
+    }
+}
