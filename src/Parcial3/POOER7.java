fca35038a06274fbcfa801eb05d2c7acdc133f7c
jberriol <jberriol@unal.edu.co>
2023-09-09 14:58:20 -0500

    Ejercicio 7 parcial 3 terminado

---
+++ src/Parcial3/POOER7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOER7 {
+    public static void main(String args[]){
+        Formulario7 form = new Formulario7();
+        form.setVisible(true);
+    }
+    
+}
