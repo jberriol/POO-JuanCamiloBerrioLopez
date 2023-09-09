c304dd3d4a605fd78ea45c92570b80430c23a735
jberriol <jberriol@unal.edu.co>
2023-09-09 16:51:15 -0500

    Ejercicio 23 terminado

---
+++ src/Parcial3/POOEP23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP23 {
+    public static void main(String args[]){
+        Formulario23 form = new Formulario23();
+        form.setVisible(true);
+    }
+}
