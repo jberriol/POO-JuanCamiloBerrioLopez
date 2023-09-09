01e93d047a75e8af86d9634131c76cf99ae0ba09
jberriol <jberriol@unal.edu.co>
2023-09-09 14:36:16 -0500

    Ejercicios 18 y 19 propuestos parcial 3

---
+++ src/Parcial3/POOEP18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP18 {
+    public static void main(String args[]){
+    Formulario18 form = new Formulario18();
+    form.setVisible(true);
+    }
+}
+    
---
+++ src/Parcial3/POOEP19.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP19 {
+    public static void main(String args[]){
+    Formulario19 form = new Formulario19();
+    form.setVisible(true);
+    }
+}
