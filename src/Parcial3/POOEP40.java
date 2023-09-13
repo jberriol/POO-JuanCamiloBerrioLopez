fdb103805e2e6ecc99a790ef886565af0cc7ad48
jberriol <jberriol@unal.edu.co>
2023-09-12 22:01:39 -0500

    Ejercicios 40 y 41 capitulo 5 terminados

---
+++ src/Parcial3/POOEP40.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP40 {
+    public static void main(String args[]){
+        Formulario40 form = new Formulario40();
+        form.setVisible(true);
+    }
+}
---
+++ src/Parcial3/POOEP41.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial3;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class POOEP41 {
+    public static void main(String args[]){
+        Formulario41 form = new Formulario41();
+        form.setVisible(true);
+    }
+    
+}
