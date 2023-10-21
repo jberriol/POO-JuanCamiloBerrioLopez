d7a664c3e12f3f600dcaa926be515fda4120a491
jberriol <jberriol@unal.edu.co>
2023-10-21 10:21:44 -0500

    Ejercico 8.2 parcial 5 terminado

---
+++ src/Parcial5/Notas/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial5.Notas;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+    
+}
