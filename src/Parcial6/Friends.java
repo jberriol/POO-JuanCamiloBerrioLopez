4163444fe1b715855fc6e13d16b152910f024b2f
jberriol <jberriol@unal.edu.co>
2023-10-28 13:45:07 -0500

    Parcial 6 terminado

---
+++ src/Parcial6/Friends.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parcial6;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class Friends {
+    public static void main(String Args[]){
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
